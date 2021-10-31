9c6c29d1f58fdb7530796f992f5c9a61380e8f33
yagya-rawal <69795983+yagya-rawal@users.noreply.github.com>
2021-10-31 23:32:45 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -17,4 +17,29 @@ void insertionSort(int arr[], int n)
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
-            j 
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+ 
+// A utility function to print an array of size n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+/* Driver code */
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    insertionSort(arr, n);
+    printArray(arr, n);
+ 
+    return 0;
+}
